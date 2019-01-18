e11f00dec3effa34806e28dead20aaff7e262445
Prashantk_Singh <pksiet09@gmail.com>
2019-01-19 03:31:25 +0530

    GetLongFromDate error fix

--- app/src/test/java/in/apollo/UtilsTest.java
+++ app/src/test/java/in/apollo/UtilsTest.java
@@ -8,9 +8,23 @@ import in.apollo.utils.Utils;
 public class UtilsTest {
 
     @Test
-    public void testLongFromDate() {
+    public void testValidInputLongFromDate() {
         String date = "2004-06-05T13:38:03-08:00";
         long time = Utils.getLongFromDate(date);
         Assert.assertTrue(time > 0);
+    }
+
+    @Test
+    public void testInvalidInputLongFromDate() {
+        String date = "2004-06-05T13";
+        long time = Utils.getLongFromDate(date);
+        Assert.assertTrue(time == 0);
+    }
+
+    @Test
+    public void testNullInputLongFromDate() {
+        String date = null;
+        long time = Utils.getLongFromDate(date);
+        Assert.assertTrue(time == 0);
     }
 }
